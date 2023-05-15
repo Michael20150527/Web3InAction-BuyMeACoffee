44365c11f34bf0a3a67711a793da9dff85d59111
Administrator <Michael-glx@163.com>
2023-05-15 20:40:18 +0800

    A dapp of buying me a coffee

---
+++ scripts/withdraw.js
@@ -0,0 +1,47 @@
+// scripts/withdraw.js
+
+const hre = require("hardhat");
+
+async function getBalance(address) {
+    const balanceBigInt = await hre.ethers.provider.getBalance(address);
+    return hre.ethers.utils.formatEther(balanceBigInt);
+}
+
+async function main() {
+  // Get the example accounts we'll be working with.
+  const [signer, ] = await hre.ethers.getSigners();
+
+  // We get the contract to deploy.
+  const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
+  const buyMeACoffee = await BuyMeACoffee.deploy();
+
+  // Deploy the contract.
+  await buyMeACoffee.deployed();
+  console.log("BuyMeACoffee deployed to:", buyMeACoffee.address);
+
+  // Check starting balances.
+  console.log("current balance of owner: ", await getBalance(signer.address), "ETH");
+  const contractBalance = await getBalance(buyMeACoffee.address);
+  console.log("current balance of contract: ", await getBalance(buyMeACoffee.address), "ETH");
+
+  // Withdraw funds if there are funds to withdraw.
+  if (contractBalance !== "0.0") {
+    console.log("withdrawing funds..")
+    const withdrawTxn = await buyMeACoffee.withdrawTips();
+    await withdrawTxn.wait();
+  } else {
+    console.log("no funds to withdraw!");
+  }
+
+  // Check ending balance.
+  console.log("current balance of owner: ", await getBalance(signer.address), "ETH");
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
